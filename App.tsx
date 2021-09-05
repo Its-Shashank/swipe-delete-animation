33533e7c465fd6a5e151d2e8196ac9d5e926432c
Shashank Gaur <shashankgaur@Shashanks-MacBook-Air.local>
2021-09-05 09:54:58 +0530

    animtions and UI done

--- App.tsx
+++ App.tsx
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, ScrollView, SafeAreaView, View } from 'react-native';
+import ListItem from './components/ListItem/ListItem';
+
+export interface Task {
+  title: string;
+  index: number;
+}
+
+const TITLES = [
+  'Here is a sample for swipe',
+  'A delete functionality',
+  'Leave a ⭐️ on the repository',
+  'Making it easy for users',
+  'Making the UI better 😎',
+];
+
+const TASKS: Task[] = TITLES.map((title, index) => ({ title, index }));
+
+const BACKGROUND_COLOR = '#FAFBFF';
 
 export default function App() {
+  const [tasks, setTasks] = useState(TASKS);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titleStyle}>Tasks</Text>
+      <ScrollView style={{ flex: 1 }}>
+        {tasks.map(task => (
+          <ListItem task={task} key={task.index} />
+        ))}
+      </ScrollView>
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: BACKGROUND_COLOR,
+  },
+  titleStyle: {
+    fontSize: 60,
+    marginLeft: '5%',
+    marginVertical: 20,
   },
 });
---
+++ components/ListItem/ListItem.tsx
@@ -0,0 +1,122 @@
+import React, { FC } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Task } from '../../App';
+
+interface ListItemProps {
+  task: Task;
+}
+
+const LIST_ITEM_HEIGHT = 70;
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const TRANSLATE_X_THRESHOLD = -SCREEN_WIDTH * 0.3;
+
+const ListItem: FC<ListItemProps> = ({ task }) => {
+  const translateX = useSharedValue(0);
+  const itemHeight = useSharedValue(LIST_ITEM_HEIGHT);
+  const marginVertical = useSharedValue(10);
+  const opacity = useSharedValue(1);
+
+  const panGesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+    onActive: e => {
+      translateX.value = e.translationX;
+    },
+    onEnd: () => {
+      if (translateX.value < TRANSLATE_X_THRESHOLD) {
+        translateX.value = withTiming(-SCREEN_WIDTH);
+        itemHeight.value = withTiming(0);
+        marginVertical.value = withTiming(0);
+        opacity.value = withTiming(0);
+        return;
+      }
+      translateX.value = withTiming(0);
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+      ],
+    };
+  });
+
+  const rIconContainerStyle = useAnimatedStyle(() => {
+    const opacity = withTiming(
+      translateX.value < TRANSLATE_X_THRESHOLD ? 1 : 0
+    );
+    return {
+      opacity,
+    };
+  });
+
+  const rTaskContainerStyle = useAnimatedStyle(() => {
+    return {
+      height: itemHeight.value,
+      marginVertical: marginVertical.value,
+      opacity: opacity.value,
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.tasksContainer, rTaskContainerStyle]}>
+      <Animated.View style={[styles.iconContainer, rIconContainerStyle]}>
+        <FontAwesome5
+          name='trash-alt'
+          size={LIST_ITEM_HEIGHT * 0.4}
+          color='red'
+        />
+      </Animated.View>
+      <PanGestureHandler onGestureEvent={panGesture}>
+        <Animated.View style={[styles.task, rStyle]}>
+          <Text>{task.title}</Text>
+        </Animated.View>
+      </PanGestureHandler>
+    </Animated.View>
+  );
+};
+const styles = StyleSheet.create({
+  tasksContainer: {
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  task: {
+    width: '90%',
+    height: LIST_ITEM_HEIGHT,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingLeft: 20,
+    // shadow for IOS
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 20 },
+    shadowRadius: 10,
+    // shadow for android
+    elevation: 5,
+    borderRadius: 10,
+  },
+  taskTitle: {
+    fontSize: 16,
+  },
+  iconContainer: {
+    height: LIST_ITEM_HEIGHT,
+    width: LIST_ITEM_HEIGHT,
+    position: 'absolute',
+    right: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ListItem;
